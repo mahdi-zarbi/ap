646ba88aeb90230c2a3010e494fa112676b7b5eb
mahdi <zarbi.mahdi0101@gmail.com>
2025-05-31 21:34:54 +0330

    1

--- 8/src/Company.java
+++ 8/src/Company.java
@@ -1,13 +1,12 @@
 public class Company {
     public static void main(String[] args) {
-        SimpleEmployye emp1 = new SimpleEmployye("علی", 30, 101, 5000);
-        SimpleEmployye emp2 = new SimpleEmployye("حسین", 28, 102, 6000);
+        SimpleEmployye emp1 = new SimpleEmployye("mahdi", 30, 101, 5000);
+        SimpleEmployye emp2 = new SimpleEmployye("ali", 28, 102, 6000);
 
-        Manager manager = new Manager("محمد", 40, 201, 10000, "مدیر بخش فناوری");
+        Manager manager = new Manager("matin", 40, 201, 10000, "Packaging Manager");
         manager.addSubordinate(emp1);
         manager.addSubordinate(emp2);
 
         manager.showDetails();
-        //System.out.println("حقوق مدیر: " + manager.());
     }
 }
