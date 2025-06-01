d67c7a02ded7ad6bd7e48aa0805f1db29b86bd6e
mahdi <zarbi.mahdi0101@gmail.com>
2025-06-02 01:29:46 +0330

    1

---
+++ 10/src/Cart.java
@@ -0,0 +1,9 @@
+public class Cart {
+    int number;
+    String typecard;
+
+    public Cart(String typecard,int number){
+        this.typecard=typecard;
+        this.number=number;
+    }
+}
--- 10/src/Main.java
+++ 10/src/Main.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args){
         //---------------Book--------------------
-        System.out.println("1.Book");
+        System.out.println("----------1.Book-----------");
         List<Book> book=new ArrayList<>();
         book.add(new Book("mahdi","sigma",1998));
         book.add(new Book("matin","tabdil",2025));
@@ -18,12 +19,43 @@ public class Main {
             System.out.println(a);
         }
         //----------------Employee-----------------
-        System.out.println("2.Employee");
+        System.out.println("------------2.Employee------------");
         List<Employee> employees=new ArrayList<>();
-        employees.add(new Employee("saman",123));
+        employees.add(new Employee("saman",128));
         employees.add(new Employee("mamad",190));
         employees.add(new Employee("masoud",123));
+
         Map<Integer,String> employee=new HashMap<>();
+        for(Employee a:employees){
+            employee.put(a.id,a.name);
+        }
+
+        for (Map.Entry<Integer, String> entry : employee.entrySet()) {
+            System.out.println("ID: " + entry.getKey() + ", Name: " + entry.getValue());
+        }
+
+        //----------------Product-----------------
+        System.out.println("-----------3.Product------------");
+        Map<String,Integer> product=new HashMap<>();
+        product.put("laptop",1999);
+        product.put("mobile",562);
+        product.put("tablet",860);
+        product.put("pc",2150);
+
+        List<String> list=new ArrayList<>();
+        for(Map.Entry<String,Integer> a: product.entrySet()){
+            if(a.getValue()>100){
+                list.add(new String(a.getKey()));
+            }
+        }
+
+        Iterator<String> it=list.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        //----------------game-----------------
+        System.out.println("-----------4.game by card------------");
 
 
     }
